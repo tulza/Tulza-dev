dc72385e7c63050da494ace87e6db5b4149c0a87
Tulza <40633228+tulza@users.noreply.github.com>
2024-07-27 02:12:50 +1200

    Added animation merging

--- src/data/variants/animations.ts
+++ src/data/variants/animations.ts
@@ -5,20 +5,33 @@ type textVariant = {
   visible: Variant;
 };
 
+export const mergeAnimation = (animations: textVariant[]) => {
+  const mergeHidden = animations.map((anim) => anim.hidden);
+  const mergeVisible = animations.map((anim) => anim.visible);
+  return {
+    hidden: Object.assign({}, ...mergeHidden),
+    visible: Object.assign({}, ...mergeVisible),
+  };
+};
 // animations
-export const blurIn: textVariant = {
-  hidden: { opacity: 0, filter: 'blur(8px)', width: 0 },
-  visible: { opacity: 1, filter: 'blur(0px)', width: 'auto' },
+const blurIn: textVariant = {
+  hidden: { filter: 'blur(8px)' },
+  visible: { filter: 'blur(0px)' },
 };
 
-export const typing: textVariant = {
+const widen: textVariant = {
+  hidden: { width: 0 },
+  visible: { width: 'auto' },
+};
+
+export const pop: textVariant = {
   hidden: { display: 'none' },
   visible: { display: 'block' },
 };
 
 export const fadeIn: textVariant = {
-  hidden: { opacity: 0, width: 0 },
-  visible: { opacity: 1, width: 'auto' },
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
 };
 
 // Transitions
@@ -29,10 +42,10 @@ export const smoothEaseInOut: Transition = {
 
 // full animation
 export const smoothTextBlur: AnimationProps | LayoutProps = {
-  variants: blurIn,
-  transition: { ...smoothEaseInOut },
+  variants: mergeAnimation([blurIn, fadeIn]),
+  transition: smoothEaseInOut,
 };
 
 export const typingAnim: AnimationProps | LayoutProps = {
-  variants: typing,
+  variants: pop,
 };
