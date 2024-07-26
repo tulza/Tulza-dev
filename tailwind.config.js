34dd5a3b9616189ae09e7da2f64026ae3c16e8ec
Tulza <40633228+tulza@users.noreply.github.com>
2024-07-27 02:09:09 +1200

    Config tsconfig pathing, minor styling changes
    - Added tsconfig path and it's vite plugin
    - hide scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,json}'],
   theme: {
-    extend: {},
+    extend: {
+      width: { desktop: '1200px' },
+    },
   },
   plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
